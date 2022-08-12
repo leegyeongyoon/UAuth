d33ac1d17bead2ab503821890e1ae3a187aab6f9
hjlee <hj.lee@okestro.com>
2022-08-12 13:10:46 +0900

    [Add] Api Result

--- src/main/java/hanacloudia/uauth/model/response/HanaApiResult.java
+++ src/main/java/hanacloudia/uauth/model/response/HanaApiResult.java
@@ -1,6 +1,25 @@
 package hanacloudia.uauth.model.response;
 
 
-public class HanaApiResult {
+import hanacloudia.uauth.entity.HanaUserEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.List;
+
+@Getter
+@Setter
+public class HanaApiResult {
+    @ApiModelProperty(value=" 응답 성공여부 : true/false ")
+    private boolean success;
+
+    @ApiModelProperty(value=" 응답 코드 ")
+    private int code;
+
+    @ApiModelProperty(value=" 응답 메시지 ")
+    private String message;
+
+    @ApiModelProperty(value=" List ")
+    private List<HanaUserEntity> list;
 }
