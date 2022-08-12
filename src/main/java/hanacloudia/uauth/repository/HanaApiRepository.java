b37476129642f57bc17c1dce27d183c652cf3de1
hjlee <hj.lee@okestro.com>
2022-08-12 13:10:14 +0900

    [Add] Api Repository

--- src/main/java/hanacloudia/uauth/repository/HanaApiRepository.java
+++ src/main/java/hanacloudia/uauth/repository/HanaApiRepository.java
@@ -3,6 +3,12 @@ package hanacloudia.uauth.repository;
 import hanacloudia.uauth.entity.HanaUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HanaApiRepository extends JpaRepository<HanaUserEntity,Long> {
+    List<HanaUserEntity> findByHanaPrtlEmpNoAndPassword(String uid, String password);
+    List<HanaUserEntity> findByHanaprtlEmpNm(String name);
+    List<HanaUserEntity> findByHanaPrtlEmpNo(String empno);
+    List<HanaUserEntity> findByEmalAdr(String email);
 
 }
